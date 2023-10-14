8f7f9ca55e6241587bca78557c5f5cfcc2cfbc5c
Tabola <tabolasenekane@gmail.com>
2023-10-14 23:47:02 +0200

    Cag Registration View Update

--- ui/app/registration/controllers/CagRegisterController.js
+++ ui/app/registration/controllers/CagRegisterController.js
@@ -7,6 +7,7 @@ angular.module('bahmni.registration')
             $scope.patientlist=[];
             // $scope.cagMembers=[];
             $scope.cag = [];
+            $scope.cag.cagPatientList=[];
             $scope.searchAddress = function(fieldName, query){
                 var parentUuid = null;
                 addressHierarchyService.search(fieldName, query, parentUuid)
@@ -24,8 +25,64 @@ angular.module('bahmni.registration')
             $scope.selectAddress = function(selectedAddress){
                 $scope.village=selectedAddress.name;
                 $scope.constituency=selectedAddress.parent.name;
+                $scope.addressResults = [];
             }
 
+            $scope.searchPatient = function(fieldPatient){
+                var apiUrl1 = Bahmni.Registration.Constants.baseOpenMRSRESTURL+'/patient?q='+fieldPatient+'&limit=10';            
+                $http.get(apiUrl1)
+                .then(function(response) {
+                    // Handle the successful response here
+                    $scope.patientResults=response.data.results;
+                })
+                .catch(function(error) {
+                    // Handle any errors that occurred during the request
+                    alert('API Error:', error);
+                });
+            }
+             
+            $scope.patientTobeAdded={};
+            
+            $scope.selectPatient = function(selectedPatient){
+                $scope.patientTobeAdded=selectedPatient;
+                $scope.newPatient = $scope.patientTobeAdded.display;
+                $scope.patientResults=[];
+            }
+
+            $scope.searchCagList = function(key, cagListLength){
+                for (let i = 0; i < cagListLength; i++) {
+                    if(key == $scope.cag.cagPatientList[i].uuid){
+                        return 1
+                    }
+                }
+                return 0;
+            }
+
+            $scope.openPatientDashboardInNewTab = function (cagMember) {
+                // var personRelatedTo = getPersonRelatedTo(cagMember);
+                console.log(getPatientRegistrationUrl(cagMember.uuid));
+                $window.open(getPatientRegistrationUrl(cagMember.uuid), '_blank');
+            };
+
+            var getPatientRegistrationUrl = function (patientUuid) {
+                return '#/patient/' + patientUuid;
+            };
+
+            $scope.removeFromCAG = function(index) {
+                $scope.cag.cagPatientList.splice(index, 1);
+            }
+
+            $scope.addPatientToCag = function(patientTobeAdded, cagListLength){
+                console.log(cagListLength);
+                if(JSON.stringify($scope.patientTobeAdded) != '{}' && $scope.searchCagList(patientTobeAdded.uuid,cagListLength)==0){
+                    $scope.cag.cagPatientList.push($scope.patientTobeAdded);
+                    $scope.patientTobeAdded = {};
+                    $scope.newPatient = '';
+                }
+                else{
+                    alert('No selected Patient...');
+                }
+            }
             
             $scope.fetchCag = function(url) {
                 $http.get(apiUrl)
@@ -55,17 +112,6 @@ angular.module('bahmni.registration')
 
 
             
-            var apiUrl1 = Bahmni.Registration.Constants.baseOpenMRSRESTURL+'/patient?q='+$scope.newField;            
-            $http.get(apiUrl1)
-                .then(function(response) {
-                    // Handle the successful response here
-                    // console.log('API Response:', response);
-                    $scope.patientlist=response.data.results;
-                    console.log('API Response:', $scope.patientlist);
-                })
-                .catch(function(error) {
-                    // Handle any errors that occurred during the request
-                    console.error('API Error:', error);
-                });
+            
         }
     ]);
